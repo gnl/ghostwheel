24c04497aefcdb4ffd4bf2bf13cc76665e75d8d0
gnl <gnl@lipov.net>
2018-07-01 19:15:39 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gnl/ghostwheel "0.2.2"
+(defproject gnl/ghostwheel "0.2.3-SNAPSHOT"
   :description "Hassle-free clojure.spec, side effect detection and evaluation tracing for Clojure(-Script)"
   :url "https://github.com/gnl/ghostwheel"
   :license {:name "Eclipse Public License"
