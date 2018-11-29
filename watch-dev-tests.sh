9de2832014e3bb57a67470cfe43e0ca6c541f147
George Lipov <gnl@lipov.net>
2018-11-29 19:45:53 +0100

    Clean up test-watching script

--- watch-dev-tests.sh
+++ watch-dev-tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 echo -e "\n### Testing Clojure build..."
-lein test ghostwheel.dev-test || exit 1
+lein with-profile +clj-dev-test test ghostwheel.dev-test
 
 echo -e "\n### Watching ClojureScript build (chrome-headless, dev environment)..."
-lein doo chrome-headless dev-test auto || exit 1
+lein doo chrome-headless dev-test auto
 
