1a587d024e82843a7d4bc51fb7f810b15a3b8ba9
George Lipov <gnl@lipov.net>
2018-11-30 03:42:42 +0100

    Clean up test script

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-echo -e "\n### Testing Clojure build..."
-lein test ghostwheel.dev-test || exit 1
+set -e
+
+echo -e "\n### Testing Clojure build (dev environment)..."
+lein with-profile +clj-dev-test test ghostwheel.dev-test
+
+echo -e "\n### Testing Clojure build (prod environment)..."
+lein test ghostwheel.prod-test
 
 echo -e "\n### Testing ClojureScript build (chrome-headless, dev environment)..."
-lein doo chrome-headless dev-test once || exit 1
+lein doo chrome-headless dev-test once
 
 echo -e "\n### Testing ClojureScript build (chrome-headless, prod environment)..."
-lein doo chrome-headless prod-test once || exit 1
+lein doo chrome-headless prod-test once
 
 echo -e "\n### Testing ClojureScript build (node, dev environment)..."
-lein doo node dev-test-node once || exit 1
+lein doo node dev-test-node once
 
 echo -e "\n### Testing ClojureScript build (node, prod environment)..."
-lein doo node prod-test-node once || exit 1
+lein doo node prod-test-node once
 
 echo -e "\n### SUCCESS\n\nAll tests passed in all environments."
