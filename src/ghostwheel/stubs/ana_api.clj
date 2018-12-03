903c080da9d1876c1c8752f0501ad1d04fedf01d
George Lipov <gnl@lipov.net>
2018-12-03 01:19:55 +0100

    Remove harmless compile warnings

--- src/ghostwheel/stubs/ana_api.clj
+++ src/ghostwheel/stubs/ana_api.clj
@@ -1,4 +1,5 @@
-(ns ^:no-doc ghostwheel.stubs.ana-api)
+(ns ^:no-doc ghostwheel.stubs.ana-api
+  (:refer-clojure :exclude [find-ns ns-interns all-ns resolve]))
 
 (defn find-ns [ns] nil)
 (defn ns-interns [ns] nil)
