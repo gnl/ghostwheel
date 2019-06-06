7f680f21f5afcb52bd8f7fa60896fae3633998e4
George Lipov <gnl@lipov.net>
2019-06-06 03:07:36 +0200

    Add copyright notices

--- src/ghostwheel/stubs/ana_api.clj
+++ src/ghostwheel/stubs/ana_api.clj
@@ -1,3 +1,11 @@
+;; Copyright (c) George Lipov. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 2.0 (https://choosealicense.com/licenses/epl-2.0/)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns ^:no-doc ghostwheel.stubs.ana-api
   (:refer-clojure :exclude [find-ns ns-interns all-ns resolve]))
 
