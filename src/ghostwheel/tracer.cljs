87ecaa934e8abbd678188d496a8fc1fa0439818d
George Lipov <gnl@lipov.net>
2019-06-08 00:25:50 +0200

    Refactor tracer

--- src/ghostwheel/tracer.cljs
+++ src/ghostwheel/tracer.cljs
@@ -21,6 +21,111 @@
 
 (def *inside-let (atom false))
 
+(defn- trace-enter
+  [{:keys [anonymous? named? arglist args dispatch-val form init name ns op protocol] :as data}
+   {:keys [color background prefix suffix expand] :as options}]
+  (let [binding-group (if (contains? expand :bindings)
+                        group
+                        group-collapsed)
+        ;init          (if (and (seq? init)
+        ;                       (= (first init) 'try)
+        ;                       (seq? (second init))
+        ;                       (-> (second init)
+        ;                           first
+        ;                           str
+        ;                           (string/starts-with? "ghostwheel.threading-macros/*")))
+        ;                nil
+        ;                init)
+        op-sym        (symbol (cljs.core/name op))
+        unnamed-fn?   (and (#{'fn 'fn*} op-sym)
+                           (not named?))
+        group         (if (contains? expand op-sym)
+                        (if unnamed-fn?
+                          group-collapsed
+                          group)
+                        group-collapsed)]
+    (cond
+      (contains? logging/fn-like-ops op)
+      (let [title (if protocol
+                    (str protocol " " name " " arglist)
+                    (str (when prefix (str prefix " – "))
+                         ns "/" (when anonymous? "__anon_") name
+                         (when dispatch-val
+                           (str " " (pr-str dispatch-val)))
+                         (str " " arglist)))]
+        (group title
+               {::logging/background background
+                ::logging/foreground color
+                ::logging/weight     "bold"}
+               80
+               suffix))
+
+      (#{'let} op)
+      (do
+        (reset! *inside-let true)
+        (group (str op)
+               {::logging/background background
+                ::logging/foreground color
+                ::logging/weight     "bold"}
+               80
+               suffix))
+
+      (#{:binding} op)
+      (let [max-length 80
+            init       (when @*inside-let
+                         (logging/truncate-string (str init) max-length))]
+        (binding-group (str form) nil nil init)
+        #_(when (> (count label) max-length)
+            (group-collapsed "...")
+            (log (with-out-str (pprint/pprint label)))
+            (group-end)))
+
+      ;(#{'->} op)
+      ;(log (str form))
+
+      :else (log (str form)))))
+
+
+(defn- trace-exit
+  [{:keys [op exit] :as data} options]
+  (cond
+    (#{:binding} op)
+    (do (log-exit exit)
+        (group-end))
+
+    (contains? logging/complex-trace-ops op)
+    (do
+      (when (#{'let `let} op)
+        (reset! *inside-let false))
+      (log-exit exit)
+      (group-end))
+
+    :else (log-exit exit)))
+
+
+(defn- trace-error
+  [{:keys [op form error ex-data] :as data} options]
+  (cond
+    (#{:binding} op)
+    (do
+      (error (.-stack error))
+      (when ex-data
+        (group-collapsed "ex-data")
+        (group-collapsed ex-data)
+        (group-end)
+        (group-end)))
+
+    (contains? logging/complex-trace-ops op)
+    (do (group-end)
+        (do
+          (error (.-stack error))
+          (when ex-data
+            (group-collapsed "ex-data")
+            (group-collapsed ex-data)
+            (group-end)
+            (group-end)))
+        (group-end))))
+
 
 (defn tracer
   "Custom tracer for Clairvoyant used by Ghostwheel but not dependent on it.
@@ -32,102 +137,11 @@
   :suffix - ...
   :expand - set of op symbols to display expanded by default. Use :bindings to expand all bindings.
   Example: #{'defn 'let :bindings}"
-  [& {:keys [color background prefix suffix expand] :as options}]
-  (let [binding-group (if (contains? expand :bindings)
-                        group
-                        group-collapsed)
-        has-bindings? (set/union logging/fn-like-ops logging/nested-binding-ops)
-        fn-like?      logging/fn-like-ops]
-    (reify
-      ITraceEnter
-      (-trace-enter
-        [_ {:keys [anonymous? named? arglist args dispatch-val form init name ns op protocol]}]
-        (let [init        (if (and (seq? init)
-                                   (= (first init) 'try)
-                                   (seq? (second init))
-                                   (-> (second init)
-                                       first
-                                       str
-                                       (string/starts-with? "ghostwheel.threading-macros/*")))
-                            nil
-                            init)
-              op-sym      (symbol (cljs.core/name op))
-              unnamed-fn? (and (#{'fn 'fn*} op-sym)
-                               (not named?))
-              group       (if (contains? expand op-sym)
-                            (if unnamed-fn?
-                              group-collapsed
-                              group)
-                            group-collapsed)]
-          (cond
-            (fn-like? op)
-            (let [title (if protocol
-                          (str protocol " " name " " arglist)
-                          (str (when prefix (str prefix " – "))
-                               ns "/" (when anonymous? "__anon_") name
-                               (when dispatch-val
-                                 (str " " (pr-str dispatch-val)))
-                               (str " " arglist)))]
-              (group title
-                     {::logging/background background
-                      ::logging/foreground color
-                      ::logging/weight     "bold"}
-                     80
-                     suffix))
+  [& options]
+  (reify
+    ITraceEnter (-trace-enter [_ data] (trace-enter data options))
 
-            (#{'let} op)
-            (do
-              (reset! *inside-let true)
-              (group (str op)
-                     {::logging/background background
-                      ::logging/foreground color
-                      ::logging/weight     "bold"}
-                     80
-                     suffix))
+    ITraceExit (-trace-exit [_ data] (trace-exit data options))
 
-            (#{'binding} op)
-            (let [max-length 80
-                  init       (when @*inside-let
-                               (logging/truncate-string (str init) max-length))]
-              (binding-group (str form) nil nil init)
-              #_(when (> (count label) max-length)
-                  (group-collapsed "...")
-                  (log (with-out-str (pprint/pprint label)))
-                  (group-end))))))
+    ITraceError (-trace-error [_ data] (trace-error data options))))
 
-      ITraceExit
-      (-trace-exit [_ {:keys [op exit]}]
-        (cond
-          (#{'binding} op)
-          (do (log-exit exit)
-              (group-end))
-
-          (has-bindings? op)
-          (do
-            (when (#{'let `let} op)
-              (reset! *inside-let false))
-            (log-exit exit)
-            (group-end))))
-
-      ITraceError
-      (-trace-error [_ {:keys [op form error ex-data]}]
-        (cond
-          (#{'binding} op)
-          (do
-            (error (.-stack error))
-            (when ex-data
-              (group-collapsed "ex-data")
-              (group-collapsed ex-data)
-              (group-end)
-              (group-end)))
-
-          (has-bindings? op)
-          (do (group-end)
-              (do
-                (error (.-stack error))
-                (when ex-data
-                  (group-collapsed "ex-data")
-                  (group-collapsed ex-data)
-                  (group-end)
-                  (group-end)))
-              (group-end)))))))
